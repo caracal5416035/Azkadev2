1d327987b77c2c3e592d88b9825a462cd921b79c
caracal5416035 <azka050511@gmail.com>
2025-05-26 12:24:00 +0700

    Update particle.js

--- particle.js
+++ particle.js
@@ -31,7 +31,7 @@ particlesJS("particles-js", {
       },
       "move": {
         "enable": true,
-        "speed": 2,
+        "speed": 5,
         "direction": "none",
         "out_mode": "bounce"
       }
