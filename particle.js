bc2dbd37cbabc20c0aef6842b9f2ababe590cf2f
caracal5416035 <azka050511@gmail.com>
2025-05-28 16:13:21 +0700

    Update particle.js

--- particle.js
+++ particle.js
@@ -5,7 +5,7 @@ particlesJS("particles-js", {
         "value": 80,
         "density": {
           "enable": true,
-          "value_area": 1100
+          "value_area": 1000
         }
       },
       "color": {
